be6581f3f3e1d905b8a1179a0f6e8de98a854c54
Parker Place <pleeplace@gmail.com>
2022-02-19 13:32:56 -0700

    redesign

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import {
   Stack,
-  Flex,
+  Button,
   Text,
   VStack,
-  Wrap,
-  Heading,
   useColorModeValue,
-  SimpleGrid,
-  StackDivider,
-  Box,
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
-
+  Tag,
+  HStack,
+  Center,
 } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
-import { useWallet } from '@web3-ui/core';
+import { ConnectWallet, useWallet } from '@web3-ui/core';
 import ScrollableTable from '@/components/new/table-expandable';
 import PageTransition from '../components/page-transitions';
 import Section from '@/components/section';
@@ -65,30 +58,46 @@ export default function Home() {
   return (
     <PageTransition>
       {/* <SubmitNotification /> */}
-      <Section>
-        <Stack direction={['column', 'row', 'row', 'row']} spacing={4}>
+      <Center justify="center">
+        <Stack justify="center" direction={['column', 'row', 'row', 'row']} spacing={4} border="2px" borderColor={useColorModeValue('neutralD.200', 'white')} p={4} rounded="lg" minW="40%">
           {/* <Hero title="Blog" /> */}
           {/* <NewsletterModal /> */}
-          <VStack>
-            <Flex justify="start" w="100%" direction="column" spacing={4} mb={[4, 8]}>
-              <Text as="h1" fontSize={['2xl', '3xl', '5xl']} fontWeight="semibold" color={useColorModeValue('neutralD.100', 'white')}>Commenda</Text>
-              <Text as="h2" fontSize={['md', 'md', 'lg']} color={useColorModeValue('neutralD.100', 'white')}>
-                Your wallets -
-                <b>{` ${Object.keys(balancesByAddress).join(', ')}`}</b>
-              </Text>
-              <Text as="h2" fontSize={['md', 'md', 'lg']} color={useColorModeValue('neutralD.100', 'white')}>
-                Your balances -
-                <b>{` $${balance.toFixed(2)}`}</b>
-              </Text>
-              <Text as="h2" fontSize={['md', 'md', 'lg']} color={useColorModeValue('neutralD.100', 'white')}>
-                {balance < 1000000 ? 'Not accredited 🙅 You have less than a million dollars. Stop being poor.' : 'Accredited :)'}
-              </Text>
-              {/* <Text as="h2" fontSize={['xs', 'sm', 'sm']} color="grey">Games from the Binance Smart Chain (BSC), Ethereum Mainnet, Ronin, Wax, Polygon, Harmony, and Hive blockchains</Text> */}
-            </Flex>
+          <VStack align="center" justify="center" borderColor="red" spacing={4}>
+            <Text as="h1" fontSize={['2xl', '3xl', '5xl']} fontWeight="semibold" color={useColorModeValue('neutralD.100', 'white')}>Commenda</Text>
+            <Text as="h2" fontSize={['md', 'md', 'xl']} fontWeight="bold" color={useColorModeValue('neutralD.100', 'white')}>
+              Your wallets
+            </Text>
+            {addresses.length === 0 && <ConnectWallet />}
+            {console.log('balances', balancesByAddress)}
+            {Object.keys(balancesByAddress).map((address) => (
+              <HStack>
+                <Tag
+                  size="lg"
+                  variant="subtle"
+                  colorScheme="green"
+                  rounded="lg"
+                >
+                  {address}
+                </Tag>
+                <Text as="h2" fontSize={['md', 'md', 'lg']} color={useColorModeValue('neutralD.100', 'white')}>
+                  $
+                  {balancesByAddress[address].toFixed(0)}
+                </Text>
+              </HStack>
+            ))}
+            <Text textAlign="center" as="h2" fontSize={['md', 'md', 'lg']} color={useColorModeValue('neutralD.100', 'white')} w="100%">
+              Total Balances:
+              <b>{` $${balance.toFixed(2)}`}</b>
+            </Text>
+            <VStack>
+              <Button isDisabled={balance < 1000000}>Mint Accredidation NFT</Button>
+              {balance < 1000000 && <Text color="red.300" fontSize="xs">You don't meet the $1M requirement</Text>}
+            </VStack>
+            {/* <Text as="h2" fontSize={['xs', 'sm', 'sm']} color="grey">Games from the Binance Smart Chain (BSC), Ethereum Mainnet, Ronin, Wax, Polygon, Harmony, and Hive blockchains</Text> */}
           </VStack>
           {/* <DynamicChatbox /> */}
         </Stack>
-      </Section>
+      </Center>
     </PageTransition>
   );
 }
